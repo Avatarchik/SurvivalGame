a3e0c7980910c5ef429ff384c2fdbe39643008a9
MechSam <samuelbrown3199@gmail.com>
2018-02-03 20:58:54 +0000

    Survival Meters

--- SurvivalGame/Assets/Scripts/PlayerScript/PlayerManager.cs
+++ SurvivalGame/Assets/Scripts/PlayerScript/PlayerManager.cs
@@ -19,7 +19,6 @@ public class PlayerManager : MonoBehaviour
     public GUISkin skin;
     public bool invOpen = false;
     private bool pickedItemFromInv = false;
-    public bool craftOpen = false;
 
     public Database database;
     public List<InventoryItem> items;
@@ -44,6 +43,8 @@ public class PlayerManager : MonoBehaviour
     public Color normalColor;
     public Color underwaterColor;
     public float waterLevel;
+
+    public GameObject anvil;
 
     void Start()
     {
@@ -205,7 +206,14 @@ public class PlayerManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.C))
         {
-            craftOpen = !craftOpen;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 3.3f))
+            {
+                Quaternion anvilRot = new Quaternion(-90f, 0f, 0f, 0f);
+                Instantiate(anvil, hit.point, anvilRot);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -248,7 +256,7 @@ public class PlayerManager : MonoBehaviour
         }
 
 
-        if(!pauseMenu && !craftOpen && !invOpen /*&& !curShip.playerAtWheel*/)
+        if(!pauseMenu && /*!craftOpen &&*/ !invOpen /*&& !curShip.playerAtWheel*/)
         {
             controller.enabled = true;
 
@@ -301,16 +309,19 @@ public class PlayerManager : MonoBehaviour
 
         if (hunger >= maxHunger && health > 0)
         {
+            hunger = maxHunger;
             health -= 1.5f * Time.deltaTime;
         }
 
         if (thirst >= maxThirst && health > 0)
         {
+            thirst = maxThirst;
             health -= 2.5f * Time.deltaTime;
         }
 
         if (coldness >= maxColdness && health > 0)
         {
+            coldness = maxColdness;
             health -= 0.5f * Time.deltaTime;
         }
 
@@ -349,7 +360,18 @@ public class PlayerManager : MonoBehaviour
         GUI.skin = skin;
 
         GUI.Box(new Rect(Screen.width / 2 - 10, Screen.height / 2 - 10, 20, 20), "");
-        GUI.Box (new Rect (0, 0, 120, 30), "Health: " + health + " / " + maxHealth);
+
+        GUI.Box(new Rect(5, Screen.height - 160, 200, 30), "");
+        GUI.Box(new Rect(5, Screen.height - 160, 200 * (health / maxHealth), 30), "Health: " + health + " / " + maxHealth);
+
+        GUI.Box(new Rect(5, Screen.height - 130, 200, 30), "");
+        GUI.Box(new Rect(5, Screen.height - 130, 200 * (hunger / maxHunger), 30), "Hunger: " + hunger + " / " + maxHunger);
+
+        GUI.Box(new Rect(5, Screen.height - 100, 200, 30), "");
+        GUI.Box(new Rect(5, Screen.height - 100, 200 * (stamina / maxStamina), 30), "Stamina: " + stamina + " / " + maxStamina);
+
+        GUI.Box(new Rect(5, Screen.height - 70, 200, 30), "");
+        GUI.Box(new Rect(5, Screen.height - 70, 200 * (coldness / maxColdness), 30), "Coldness: " + coldness + " / " + maxColdness);
 
         if (pauseMenu)
         {
@@ -390,15 +412,6 @@ public class PlayerManager : MonoBehaviour
                 GUI.Label(new Rect(e.mousePosition.x + 15, e.mousePosition.y, 35, 35), draggedItem.stack.ToString(), skin.GetStyle("CustomLabel"));
             }
         }
-
-        if (craftOpen)
-        {
-            controller.enabled = false;
-
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-
     }
 
     void DrawInventory()
